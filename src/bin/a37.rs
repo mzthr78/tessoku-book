da68f4f77dcb2f014e5d5c99207de13211b7d6f5
Mizuta Hiro <mzthr78@gmail.com>
2023-03-23 00:48:22 +0900

    add a37.rs

---
+++ src/bin/a37.rs
@@ -0,0 +1,26 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input!{
+        n: usize,
+        m: usize,
+        b: usize,
+        a: [usize; n],
+        c: [usize; m],
+    }
+
+    let mut ans: usize = 0;
+
+    for i in 0..n {
+        ans += a[i] * m;
+    }
+
+    ans += b * n * m;
+
+    for i in 0..m {
+        ans += c[i] * n;
+    }
+
+    println!("{}", ans);
+}
