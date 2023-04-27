3189248f0f0e82c0f6449047bb7dcf4463d4757d
Mizuta Hiro <mzthr78@gmail.com>
2023-04-27 17:43:02 +0900

    add a70b.rs

--- src/bin/a70a.rs
+++ src/bin/a70a.rs
@@ -25,25 +25,31 @@ fn main() {
         b[i] |= 1<<(n-z);
     }
 
+    let mut g: Vec<Vec<usize>> = vec![vec![]; 1<<n];
+
+    for i in 0..1<<n {
+        for j in 0..m {
+            g[i].push(i ^ b[j]);
+        }
+    }
+
     let mut q: VecDeque<usize> = VecDeque::new();    
-    let mut d = vec![0; 1<<n];
+    let mut d = vec![-1; 1<<n];
 
     q.push_back(s);
-    d[s] = 1;
+    d[s] = 0;
 
     while !q.is_empty() {
         let p = q.pop_front().unwrap();
 
-        for i in 0..m {
-            let n = p ^ b[i];
-
-            if d[n] == 0 {
-                d[n] = d[p] + 1;
-                q.push_back(n);
+        for i in 0..g[p].len() {
+            if d[g[p][i]] < 0 {
+                d[g[p][i]] = d[p] + 1;
+                q.push_back(g[p][i]);
             }
         }
     }
 
-    println!("{}", (d[(1<<n)-1] as isize) - 1);
+    println!("{}", d[(1<<n)-1]);
 }
 
