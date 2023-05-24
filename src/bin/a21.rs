5cdf0a8b80a06b12d003d71c27264f789bd04c04
Mizuta Hiro <mzthr78@gmail.com>
2023-05-24 19:13:56 +0900

    add b21

--- src/bin/a21.rs
+++ src/bin/a21.rs
@@ -1,49 +1,47 @@
-use proconio::input;
-use std::cmp;
+use proconio::{fastout, input};
+use std::cmp::max;
 
+#[fastout]
 fn main() {
-    input! {
+    input!{
         n: usize,
-        pa: [(usize, usize); n],
+        mut pa: [(usize, usize); n],
     }
+    pa.insert(0, (0, 0));
 
-    let mut dp = vec![vec![0; n]; n];
+    let mut dp = vec![vec![0; n+1]; n+1];
+    
+    for i in 1..=n {
+        for j in (i..=n).rev() {
+            let mut sl = 0;
+            let mut sr = 0;
 
-    dp[0][n-1] = 0;
-    for l in 0..n {
-        for r in (l..n).rev() {
-            //println!("({}, {})", l, r);
+            if j < n {
+                let cr = j + 1;
+                sr = dp[i][cr];
 
-            let mut lscore = 0;
-            let mut rscore = 0;
-
-            if l > 0 && l <= pa[l-1].0-1 && r >= pa[l-1].0-1 {
-                lscore = pa[l-1].1;
+                if i <= pa[cr].0 && pa[cr].0 <= j {
+                    sr += pa[cr].1;
+                }
             }
 
-            if r < n - 1 && l < pa[r+1].0 && r >= pa[r+1].0-1  {
-                rscore = pa[r+1].1;    
+            if i > 1 {
+                let cl = i - 1;
+                sl = dp[cl][j];
+
+                if i <= pa[cl].0 && pa[cl].0 <= j {
+                    sl += pa[cl].1;
+                }
             }
 
-            if r == n - 1 {
-                if l > 0 { dp[l][r] = dp[l-1][r]+lscore; }
-            } else if l == 0 {
-                dp[l][r] = dp[l][r+1]+rscore;
-            } else {
-                dp[l][r] = cmp::max(dp[l][r+1]+rscore, dp[l-1][r]+lscore);
-            }
+            dp[i][j] = max(sr, sl);
         }
     }
 
-    /*
-    for i in 0..n {
-        println!("{:?}", dp[i]);
+    let mut ans = 0;
+    for i in 1..=n {
+        ans = max(ans, dp[i][i]);
     }
-    */
 
-    let mut answer = 0;
-    for i in 0..n {
-        answer = cmp::max(answer, dp[i][i]);
-    }
-    println!("{}", answer);
+    println!("{}", ans);
 }
